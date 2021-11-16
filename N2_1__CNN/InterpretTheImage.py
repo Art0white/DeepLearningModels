f479508798a9846d178285c95528df06f3bdbda2
Art0white <840018730@qq.com>
2021-11-16 22:06:25 +0800

    2021.11.16 22:06

--- N2_1__CNN/InterpretTheImage.py
+++ N2_1__CNN/InterpretTheImage.py
@@ -11,9 +11,29 @@ import numpy as np
 from skimage.io import imread
 from skimage.transform import resize
 
-# Load a color image in grayscale
-image = imread('img/testimg.jpg', as_gray=True)
-image = resize(image, (80, 50), mode='reflect')
+# 图像可视化
+image = imread('img/testimg.png', as_gray=True)
+image = resize(image, (28, 28), mode='reflect')
 print('This image is: ', type(image),
       'with dimensions:', image.shape)
 plt.imshow(image, cmap='gray')
+
+# 图像像素值可视化(黑 0.00, 白 1.00)
+def visualize_input(img, ax):
+      ax.imshow(img, cmap='gray')
+      width, height = img.shape
+      thresh = img.max()/2.5
+      for x in range(width):
+            for y in range(height):
+                  ax.annotate(str(round(img[x][y], 2)), xy=(y, x),
+                  horizontalalignment='center',
+                  verticalalignment='center',
+                  color='white' if img[x][y] < thresh else 'black')
+
+fig = plt.figure(figsize=(12, 12))
+ax = fig.add_subplot(111)
+visualize_input(image, ax)
+
+
+plt.show()
+# 不必 import matplotlib.pyplot as plt 后面加 %matplotlib inline, 在最后加 plt.show 即可
